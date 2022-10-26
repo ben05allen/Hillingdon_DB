1d1bedf0bd33bf33fc86617b46ed4af9fd5f8b2b
Ben <ben05allen@hotmail.com>
2022-10-26 18:33:31 +0900

    adding dependency injection

--- database.py
+++ database.py
@@ -1,7 +1,46 @@
-from sqlmodel import create_engine
+from datetime import date, timedelta
+from typing import Optional, List
+from sqlmodel import SQLModel, create_engine, Field, Relationship
 
 
-sqlite_filename = "database.db"
+sqlite_filename = "hillingdon.db"
 sqlite_url = f"sqlite:///{sqlite_filename}"
 engine = create_engine(sqlite_url, echo=False)
 
+
+class Rider(SQLModel, table=True):
+    rider_id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    isJunior: bool
+    isFemale: bool
+
+    results: List['EventResult'] =  Relationship(back_populates='rider')
+
+
+class Event(SQLModel, table=True):
+    event_id: int = Field(default=None, primary_key=True)
+    date: date
+
+    results: List['EventResult'] = Relationship(back_populates='event')
+
+
+class EventResult(SQLModel, table=True):
+    results_id: Optional[int] = Field(default=None, primary_key=True)
+    event_id: Optional[int] = Field(default=None, foreign_key='event.event_id')
+    rider_id: Optional[int] = Field(default=None, foreign_key='rider.rider_id')
+    category: str
+    total_time: timedelta
+    fastest_lap_time: Optional[timedelta] = Field(default=None)
+    fastest_lap: Optional[int] = Field(default=None)
+    laps_completed: int
+
+    event: Optional[Event] = Relationship(back_populates='results')
+    rider: Optional[Rider] = Relationship(back_populates='results')
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+    
+
+if __name__ == '__main__':
+    create_db_and_tables()
--- main.py
+++ main.py
@@ -1,81 +1,103 @@
 import sys
+import json
 import datetime
-from fastapi import FastAPI, Path, Query, HTTPException, status
+import pathlib
+from typing import List, Union
 
-from models import SQLModel
-from database import engine
+from fastapi import FastAPI, Depends, Response
+from sqlmodel import Session, select
+from database import engine, Rider, Event, EventResult
 
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
 
 app = FastAPI()
+home_dir = pathlib.Path('.')
+
+
+# helper for dependency injection
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+@app.on_event('startup')
+async def startup_event():
+    with Session(engine) as session:
+        statement = select(Event)
+        result = session.exec(statement).first()
+        if result is None:
+            events_path = home_dir / 'data' / 'event_dates.json'
+            with open(events_path) as events_file:
+                events = json.load(events_file)['data']
+                for event in events:
+                    session.add(Event(**event))
+        session.commit()
+
+        statement = select(Rider)
+        result = session.exec(statement).first()
+        if result is None:
+            riders_path = home_dir / 'data' / 'riders.json'
+            with open(riders_path) as riders_file:
+                riders = json.load(riders_file)['data']
+                for rider in riders:
+                    session.add(Rider(**rider))
+        session.commit()
+
+
+
+@app.get('/')
+def home():
+    return {'Data': 'Testing'}
+
+    
+@app.get('/events/', response_model=List[Event])
+def select_events(session: Session = Depends(get_session)):
+    statement = select(Event)
+    result = session.exec(statement).all()
+    return result
+
+
+@app.get('/event/{event_id}', response_model=Union[Event, str])
+def event(event_id: int, response: Response, session: Session = Depends(get_session)):
+    track = session.get(Event, event_id)
+    if track is None:
+        response.status_code = 404
+        return "Track no found"
+    return track
+
+
+@app.get('/riders/', response_model=List[Rider])
+def select_riders(session: Session = Depends(get_session)):
+    statement = select(Rider)
+    result = session.exec(statement).all()
+    return result
+
+
+@app.get('/rider/{rider_id}', response_model=Union[Rider, str])
+def rider(rider_id: int, response: Response, session: Session = Depends(get_session)):
+    track = session.get(Rider, rider_id)
+    if track is None:
+        response.status_code = 404
+        return "Track no found"
+    return track
+
+
+
+# @app.post('/create-rider/{rider_id}')
+# def create_rider(rider_id: int, rider: Rider):
+#     if rider_id in rider:
+#         return {'Error': f'Rider ID {rider_id} already exists'}
+    
+#     riders[rider_id] = rider
+#     return riders[rider_id]
+
+
 
 
 def main():
-
-
-    @app.get('/')
-    def home():
-        return {'Data': 'Testing'}
-
-    events = {
-        1: {
-            'Title': 'Event 1',
-            'Date': datetime.date(2022, 5, 4)
-        },
-        2: {
-            'Title': 'Event 2',
-            'Date': datetime.date(2022, 5, 18)
-        },
-        3: {
-            'Title': 'Event 3',
-            'Date': datetime.date(2022, 6, 1)
-        },
-        4: {
-            'Title': 'Event 4',
-            'Date': datetime.date(2022, 6, 15)
-        },
-        5: {
-            'Title': 'Event 5',
-            'Date': datetime.date(2022, 6, 29)
-        },
-        6: {
-            'Title': 'Event 6',
-            'Date': datetime.date(2022, 7, 13)
-        },
-        7: {
-            'Title': 'Event 7',
-            'Date': datetime.date(2022, 7, 27)
-        },
-        8: {
-            'Title': 'Event 8',
-            'Date': datetime.date(2022, 8, 10)
-        },
-        9: {
-            'Title': 'Event 9',
-            'Date': datetime.date(2022, 8, 24)
-        }
-    }
-
-    riders = {}
-
-    @app.get('/get-event/{event_id}')
-    def get_event(event_id: int = Path(None, description='The ID of the event', gt=0, le=max(events))):
-        if event_id not in events:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Event {event_id} not found.')
-        
-        return events[event_id]
-
-    @app.post('/create-rider/{rider_id}')
-    def create_rider(rider_id: int, rider: Rider):
-        if rider_id in rider:
-            return {'Error': f'Rider ID {rider_id} already exists'}
-        
-        riders[rider_id] = rider
-        return riders[rider_id]
+    pass
         
 if __name__ == '__main__':
-   
+
     if '--help' in sys.argv:
         print('Usage: uvicorn main:app --reload')
 
